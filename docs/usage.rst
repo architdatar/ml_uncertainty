9bde7318a60557807baff69595a0bb7a94889f95
architdatar <archit.datar@celanese.com>
2023-07-13 17:15:24 -0400

    First commit

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use ML Uncertainty Quantification in a project::
+
+    import ml_uncertainty
---
+++ ml_uncertainty/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for ML Uncertainty Quantification."""
+
+__author__ = """Archit Nikhil Datar"""
+__email__ = 'architdatar@gmail.com'
+__version__ = '0.1.0'
