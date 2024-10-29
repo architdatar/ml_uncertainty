f4dc33c81cebd56e24256b9b80bc128a212076f9
Archit Datar <datar.10@osu.edu>
2024-10-28 20:14:53 -0400

    Working version for test release

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/architdatar/ml_uncertainty',
-    version='0.1.0-wheel',
+    version='0.1.0a1',
     zip_safe=False,
 )
